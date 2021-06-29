67ec125aa888727ffc9f9e9c2b489fa148525441
Narul <naruln9494@gmail.com>
2021-06-29 23:42:41 +0600

    floating

---
+++ Algorithms/floating.py
@@ -0,0 +1,9 @@
+x = float(input("Insert floating point numbers only: "))
+y = str(x)
+maxi = -1
+for i in range(len(y)):
+    if y[i] == '.':
+        continue
+    elif maxi < int(y[i]):
+        maxi = int(y[i])
+print("The maximum element is = ",maxi)
